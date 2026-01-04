860405b4d56e838a8dbe3ab46f40fc28fe3d0c9d
Erick Briones <erickalan518@gmail.com>
2026-01-03 23:55:40 -0600

    Builder

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,76 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+    public cpu: string = 'cpu - not defined';
+    public ram: string = 'ram - not defined';
+    public storage: string = 'storage - not defined';
+    public gpu?: string;
+
+    displayConfiguration() {
+        console.log(`Configuración de la computadora
+            CPU: ${ this.cpu }
+            RAM: ${ this.ram }
+            ALMACENAMIENTO: ${ this.storage }
+            GPU: ${ this.gpu ?? 'gpu - not defined' }
+        `);
+    }
+}
+
+class ComputerBuilder {
+    private computer: Computer;
+
+    constructor() {
+        this.computer = new Computer();
+    }
+
+    setCPU(cpu: string): ComputerBuilder {
+        this.computer.cpu = cpu;
+        return this;
+    }
+    
+    setRAM(ram: string): ComputerBuilder {
+        this.computer.ram = ram;
+        return this;
+    }
+    
+    setStorage(storage: string): ComputerBuilder {
+        this.computer.storage = storage;
+        return this;
+    }
+    
+    setGPU(gpu: string): ComputerBuilder {
+        this.computer.gpu = gpu;
+        return this;
+    }
+    
+    build() {
+        return this.computer;
+    }
+}
+
+function main() {
+    const basicComputer: Computer = new ComputerBuilder()
+        .setCPU('Intel Core I3 4200')
+        .setRAM('4GB')
+        .setStorage('256GB')
+        .build();
+        
+        const gamerComputer: Computer = new ComputerBuilder()
+            .setCPU('Intel Core I9 14500H')
+            .setRAM('64GB')
+            .setStorage('2TB')
+            .setGPU('RTX 5090 TI')
+            .build();
+        
+    console.log('%cComputadora básica: ', COLORS.blue);
+    basicComputer.displayConfiguration();
+
+    console.log('%cComputadora gamer: ', COLORS.cyan);
+    gamerComputer.displayConfiguration();
+}
+
+main();
