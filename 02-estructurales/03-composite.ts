7d8e317b47e5051e3e3a8cb9bbb3314fd5a112c4
Erick Briones <erickalan518@gmail.com>
2026-01-16 14:24:02 -0600

    Composite

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,69 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+    showDetails(indent?: string): void;
+}
+
+class File implements FileSystemComponent {
+
+    private name: string;
+    
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    showDetails(indent?: string): void {
+        console.log(`${indent}- ${this.name}`);
+    }
+
+}
+
+class Folder implements FileSystemComponent {
+    
+    private name: string;
+    private contents : FileSystemComponent[] = [] 
+
+    constructor(name: string ) {
+        this.name = name;
+    }
+
+    add(component: FileSystemComponent): void {
+        this.contents.push(component);
+    }
+
+    showDetails(indent: string = ''): void {
+      console.log(`${indent}+ ${this.name}`)
+      this.contents.forEach(component => {
+        component.showDetails(indent + ' '.repeat(2));
+      });
+    }
+}
+
+function main() {
+    const readme = new File('README.md');
+    const viyi = new File('Viyi.jpg');
+    const spotify = new File('spotify.exe');
+    const stardewValley = new File('stardewvalley.exe');
+
+    const programs = new Folder('Programs');
+    programs.add(spotify);
+    programs.add(stardewValley);
+
+    const pictures = new Folder('Pictures');
+    pictures.add(viyi);
+
+    const documents = new Folder('Documents');
+    documents.add(readme);
+
+    const rootFolder = new Folder('C://');
+
+    rootFolder.add(programs);
+    rootFolder.add(pictures);
+    rootFolder.add(documents);
+
+    rootFolder.showDetails();
+}
+
+main();
--- 02-estructurales/03.2-composite.ts
+++ 02-estructurales/03.2-composite.ts
@@ -25,13 +25,10 @@ interface MenuComponent {
 // 2. Clase MenuItem
 // Representa un ítem individual del menú, como un platillo o una bebida.
 class MenuItem implements MenuComponent {
-  private name: string;
-  private price: number;
-
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
+  constructor(
+    private name: string, 
+    private price: number
+  ) {}
 
   showDetails(indent: string = ''): void {
     console.log(
@@ -44,24 +41,28 @@ class MenuItem implements MenuComponent {
 // 3. Clase MenuCategory
 // Representa una categoría de menú que puede contener otros ítems o subcategorías.
 class MenuCategory implements MenuComponent {
-  // TODO: Crear dos propiedades privadas: name y items
-  // Name sting y items arreglo de MenuComponent
-  // Name es recibida en el constructor, items se inicializa como un arreglo vacío
+  constructor(
+    private name: string,
+    private items: MenuComponent[] = []
+  ) {}
 
-  //TODO: Sobrecarga de operadores - Item puede ser MenuComponent o un arreglo de MenuComponent
-  add(item: unknown): void {
-    // TODO: Implementar la sobrecarga de operadores
-    throw new Error('Method not implemented.');
+  add(item: MenuComponent | MenuComponent[]): void {
+    if (Array.isArray(item)) {
+      this.items.push(...item);
+    } else {
+      this.items.push(item);
+    }
   }
 
   showDetails(indent: string = ''): void {
     console.log(`%c${indent}+ ${this.name}`, COLORS.blue);
-    // TODO: Implementar foreach
+    this.items.forEach(item => {
+      item.showDetails(indent + ' '.repeat(4));
+    });
   }
 }
 
 // 4. Código Cliente para Probar el Composite
-// TODO: en esta función main, no deben de hacer nada, al ejecutarla,
 // Deben de ver la gráfica correcta del menú
 function main() {
   // Crear ítems individuales
@@ -81,8 +82,15 @@ function main() {
   mainCourse.add(steak);
 
   const beverages = new MenuCategory('Bebidas');
-  beverages.add(soda);
-  beverages.add(coffee);
+
+  const hotBeverages = new MenuCategory('Calientes');
+  beverages.add(hotBeverages);
+
+  const coldBeverages = new MenuCategory('Frías');
+  beverages.add(coldBeverages);
+
+  coldBeverages.add(soda);
+  hotBeverages.add(coffee);
 
   const desserts = new MenuCategory('Postres');
   desserts.add(dessert);
